c8631579fd78de939843ab40abadd63c96f99fad
Elie Hymowitz <ehymowitz@gmail.com>
2020-07-29 14:15:18 -0400

    added sass, cleaned up a bit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,6 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
-
 module.exports = {
-  /* Your site config here */
   plugins: [
+    `gatsby-plugin-sass`,
     {
       resolve: 'gatsby-source-contentful',
       options: {
