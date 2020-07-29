e5a8cbc8fa7e1b0a0aa4c96983657af65bf3e0ae
Elie Hymowitz <ehymowitz@gmail.com>
2020-07-29 14:05:28 -0400

    read only token used in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
       resolve: 'gatsby-source-contentful',
       options: {
         spaceId: "oeviph464mr3",
-        accessToken: process.env.CONTENTFUL_API_TOKEN
+        accessToken: 'cM8KDAPGKAZYlFAGCDzVU0_w7bTEWfBJUd88KOPZK9w',
       }
     }
   ],
