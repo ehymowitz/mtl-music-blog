2e5ebb811b17f7da586a1fd4a1c86a398b4e5bd0
Elie Hymowitz <ehymowitz@gmail.com>
2020-08-09 15:08:16 -0400

    changed form to contribute

--- src/components/fixed/top-nav.js
+++ src/components/fixed/top-nav.js
@@ -10,7 +10,7 @@ export default function TopNav() {
         <h1>THE SOUND GAP</h1>
       </Link>
       <Link to="/about">ABOUT</Link>
-      <Link to="/contact">CONTACT</Link>
+      <Link to="/contact">CONTRIBUTE</Link>
     </div>
   )
 }
