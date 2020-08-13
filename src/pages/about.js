94ad1f3dba7f14a3266eaeab270cd1c3d90e1534
Elie Hymowitz <ehymowitz@gmail.com>
2020-08-13 12:52:36 -0400

    removed scrollbars on firefox

--- src/pages/about.js
+++ src/pages/about.js
@@ -27,7 +27,8 @@ export default function About({data}) {
                 alt={data.elie.edges[0].node.name}
                 style={{margin: "30px", overflow: "hidden"}}
               />
-              <h3>{data.elie.edges[0].node.name}</h3>
+              <small style={{position: "relative", bottom:"30px", fontFamily: "Ubuntu, sans-serif", color: "grey"}}>photo cred to Terry Hughes</small>
+              <h3>{data.elie.edges[0].node.name.toUpperCase()}</h3>
               <p>
                 Hi! I'm Elie. Ayla and I are working on "The Sound Gap" together. I'm in charge of making the website, so any thing you like on here, feel free to let me know! I'm a working musician in Montreal by night and a web developer by day. To see more of my coding work check out <a href="https://www.hymowitz.org">my portfolio</a>, and you'll see some of the music I play too! Happy reading :)
               </p>
@@ -38,7 +39,7 @@ export default function About({data}) {
                 alt={data.ayla.edges[0].node.name}
                 style={{margin: "30px", overflow: "hidden"}}
               />
-              <h3>{data.ayla.edges[0].node.name}</h3>
+              <h3>{data.ayla.edges[0].node.name.toUpperCase()}</h3>
               <p>
                 Hey! I'm Ayla, I am the founder and co-creator of "The Sound Gap". I wanted to create a platform/ collective where we get to know the people behind the music, beyond projections, and not-interesting questions, and to bridge the gap between what is heard and what is created. I am a musician and studied Music Technology at school. Currently singing/ songwriting. "The Sound Gap" for me is a way to share the stories and intentions behind music and musicians I love, to spark stimulating conversations about music, and to have an excuse to listen to more music :)
               </p>
